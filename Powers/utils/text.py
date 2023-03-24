5eed566d1b59e6a9e2001d554c11ed6d0ff460f9
ChimkuOp <104584474+ChimkuOp@users.noreply.github.com>
2023-03-24 19:09:05 +0530

    Add files via upload

---
+++ Powers/utils/text.py
@@ -0,0 +1,11 @@
+from KeysSecret import COIN_EMOJI, COIN_MESSAGE, COIN_NAME, NUMBER_MESSAGE
+
+help_txt = f"""
+Type /link in this chat to get invite link .
+Share the invite link to your friends 
+every member that joins with your link will reward you some chimxions (coins) which will further used to buy stuffs.
+/mylink : To get previous genrated link of the chat by you.
+/myprofile: To get you information and available chimxions
+/premium: To get link of premium channel.
+/buy : To buy other stuffs
+"""
